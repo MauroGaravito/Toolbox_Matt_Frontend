01165cd52df711e274f88122174b9d773446f080
maurogaravito <m.garavito82@gmail.com>
2025-04-05 10:52:00 +1100

    🧼 Forzar redeploy cache-buster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme"; // Asegurate de que la ruta sea correcta
 
+console.log("🧼 Forced redeploy cache-buster");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
